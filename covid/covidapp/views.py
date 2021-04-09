98037605e652df93a97e983cd67e1de9adbd84f3
darshjain <darshj786@gmail.com>
2021-04-09 12:03:26 +0530

    Initialise

---
+++ covid/covidapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+import requests
+import json
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "b325bb7db1msh7800dc62c95cd3cp1fa727jsn19cdd173698e",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers).json()
+
+
+def covidtrackerview(request, *args, **kwaargs):
+    # context = {'response':response['response'][0]}
+    noofresults = int(response['results'])
+    country_list = []
+    for x in range(0, noofresults):
+        # print(response['response'][x]['country'])
+        country_list.append(response['response'][x]['country'])
+    if request.method == "POST":
+        selected_country = request.POST["selected_country"]
+        print(selected_country)
+        for x in range(0, noofresults):
+            if selected_country == response['response'][x]['country']:
+                active_cases = response['response'][x]['cases']['active']
+                new_cases = response['response'][x]['cases']['new']
+                total_cases = response['response'][x]['cases']['total']
+                critical_cases = response['response'][x]['cases']['critical']
+                recovered_cases = response['response'][x]['cases']['recovered']
+                deaths = int(total_cases)-int(active_cases) - \
+                    int(recovered_cases)
+        context = {'selected_country': selected_country, 'country_list': country_list, 'new': new_cases, 'active': active_cases, 'total': total_cases,
+                   'recovered': recovered_cases, 'deaths': deaths, 'critical': critical_cases}
+    # print(response['response'])
+        return render(request, 'index.html', context)
+    for x in range(0, noofresults):
+        # print(response['response'][x]['country'])
+        country_list.append(response['response'][x]['country'])
+    context = {'country_list': country_list}
+    return render(request, 'index.html', context)
