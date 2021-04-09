103d4bdca77184f9930969752e34124900cd3a23
darshjain <darshj786@gmail.com>
2021-04-09 14:31:49 +0530

    push

--- covid/covid/settings.py
+++ covid/covid/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0-*)2z+j22s@h1qa_83x7u&k)++51q^l#0^(se)pej@56xvkby'
+# SECRET_KEY = '0-*)2z+j22s@h1qa_83x7u&k)++51q^l#0^(se)pej@56xvkby'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['*']
 
 
 # Application definition
